11dea9f96756ad12805870baebf5472f628a77fa
Mandy Miller <mandy@cullenmiller.com>
2024-01-16 14:22:58 +0000

    adding questions.js to repo. Should be final version, unless need to amend

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+// CREATE AN ARRAY OF QUESTIONS
+// Questions will be objects. Each object has a question (string), multiple choice answers (array), and a correct answer indicated by its index in the choices array
+var questions = [
+    {   questionNo1: "Commonly used data types DO NOT include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        correct:[2]
+    },
+
+    {   questionNo2: "The condition in an IF/ELSE statment is enclosed in:",
+        choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+        correct:[1]
+    },
+
+    {   questionNo3: "Arrays in Javascript can be used to store:",
+        choices: ["Numbers and strings", "Other Arrays", "Booleans", "All of the above"],
+        correct:[3]
+    },
+    
+    {   questionNo3: "String values must be enclosed in ______ when being assigned to variables:",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        correct:[2]
+    },
+    
+    {   questionNo3: "A useful tool in development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal Bash ", "For Loops", "Console.log"],
+        correct:[3]
+},
+
+]
